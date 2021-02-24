d3fa65da4f2e90900314977b7749ff4e187eb14d
Rafael Bernardi <rafael.fbernardi@gmail.com>
2021-02-24 12:17:43 -0300

    Finalizado

--- scripts.js
+++ scripts.js
@@ -5,9 +5,12 @@ const cards = document.querySelectorAll('.card');
 for (let card of cards) {
 card.addEventListener("click", function(){
     const imgSrc = card.querySelector('img').src
+    const card_title = card.querySelector('.card__title p').textContent
+    const card_autor = card.querySelector('.card__autor p').textContent
     modalOverlay.classList.add('active')
     modalOverlay.querySelector("img").src = imgSrc
-    modalOverlay.querySelector(".modal-content").insertAdjacentElement("beforeend", card.querySelector('.card__title'))
+    modalOverlay.querySelector("#title").innerHTML = card_title
+    modalOverlay.querySelector("#autor").innerHTML = card_autor
 
 })
 }
